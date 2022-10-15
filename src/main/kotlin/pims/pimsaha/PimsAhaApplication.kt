703cf6a7d3de26363d7651a7c379cc2af5517173
eunjin <wlq9541@nate.com>
2022-10-15 23:57:40 +0900

    최초 commit

---
+++ src/main/kotlin/pims/pimsaha/PimsAhaApplication.kt
@@ -0,0 +1,11 @@
+package pims.pimsaha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PimsAhaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PimsAhaApplication>(*args)
+}
